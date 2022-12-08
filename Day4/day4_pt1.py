f1d5b43ebe10fd29c9055a27aa37438987495c6f
gkwityn <gkwityn@gmail.com>
2022-12-07 23:30:56 -0500

    Completed Day4 pt1

--- Day4/day4_pt1.py
+++ Day4/day4_pt1.py
@@ -1,20 +1,47 @@
 def read_data():
-    path = './test.txt'
+    #path = './test.txt'
+    path = 'data.txt'
     file = open(path, 'r', encoding='UTF-8')
     lines = file.readlines()
     return lines
-def a_subset_b(a, b):
-    return a.issubset(b)
+
+    
+def is_subset(A, B):
+
+    if A.issubset(B):
+        return True
+    elif B.issubset(A):
+        return True
+    else:
+
+        return False
+
+def build_list(section):
+    my_list = []
+    str_list = section.strip().split('-')
+    int_list = list(map(int, str_list))
+    
+    start = int_list[0]
+    end = int_list[1] + 1
+    for i in range(start, end):
+        my_list.append(i)
+    return my_list
+
 
 def main():
     lines = read_data()
+    total = 0
 
     for line in lines:
         sections = line.strip().split(sep=',')
         print(f'A: {sections[0]}, B:{sections[1]}')
-        A = set(sections[0])
-        B = set(sections[1])
+    
+        A = build_list(sections[0])
+        B = build_list(sections[1])
 
-        if a_subset_b(A,B):
-            pass
+        if is_subset(set(A),set(B)):
+            total += 1        
+
+    print(f'Total: {total}')
+
 main()
