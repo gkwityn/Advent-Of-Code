70402827a018d64d3d254f41c46ef5838542624f
gkwityn <gkwityn@gmail.com>
2022-12-09 11:50:43 -0500

    Starting build cargo obj method

--- Day5/day5_pt1.py
+++ Day5/day5_pt1.py
@@ -1,6 +1,14 @@
 class cargo:
     state = ''
     row_size = -1
+    
+    def build_structure(self):
+        stacks = []
+        lines = self.state.split('\n')
+
+        for line in lines:
+            crate = line.strip().split(' ')
+
 
 def get_stack_state(file):
     init_cargo = cargo()
@@ -21,19 +29,19 @@ def get_stack_state(file):
 
     return init_cargo
 
+
 def read_initial_struct():
     #path = './test.txt'
     path = './data.txt'
     file = open(path, 'r', encoding='UTF-8')
-    
-    init_cargo = get_stack_state(file)
-    
-    return init_cargo
+     
+    return get_stack_state(file)
 
 
 def main():
     
     my_cargo = read_initial_struct()
+    my_cargo.build_structure()
     print(f'State:\n{my_cargo.state}')
     print(f'RowSize: {my_cargo.row_size}')
 
