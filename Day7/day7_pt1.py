0f7712356168bc95aa05ae2114dc8030ecac15d3
gkwityn <gkwityn@gmail.com>
2022-12-12 15:10:54 -0500

    Starting Day 7 pt 1

---
+++ Day7/day7_pt1.py
@@ -0,0 +1,34 @@
+# - / (dir)
+#   - a (dir)
+#     - e (dir)
+#       - i (file, size=584)
+
+#     - f (file, size=29116)
+#     - g (file, size=2557)
+#     - h.lst (file, size=62596)
+
+#   - b.txt (file, size=14848514)
+#   - c.dat (file, size=8504156)
+
+#   - d (dir)
+#     - j (file, size=4060174)
+#     - d.log (file, size=8033020)
+#     - d.ext (file, size=5626152)
+#     - k (file, size=7214296)
+
+
+
+
+from anytree import Node, RenderTree
+
+
+root = Node("root")
+marc = Node("Marc", parent=udo)
+lian = Node("Lian", parent=marc)
+dan = Node("Dan", parent=udo)
+jet = Node("Jet", parent=dan)
+jan = Node("Jan", parent=dan)
+joe = Node("Joe", parent=dan)
+
+for pre, fill, node in RenderTree(udo):
+    print("%s%s" % (pre, node.name))
