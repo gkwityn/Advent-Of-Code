03ddeb118091245917a78e72bb97098da2689029
gkwityn <gkwityn@gmail.com>
2023-01-10 16:15:27 -0500

    parse_input and print_state methods complete

--- Day9/day9_pt1.py
+++ Day9/day9_pt1.py
@@ -21,35 +21,86 @@ def parse_input(path):
 def get_grid_size(lines):
     x_max = 0
     y_max = 0
+    x_pos = 0
+    y_pos = 0
 
     for line in lines:
         l = line.split()
-        
-        if l[0] == 'R':
-            if int(l[1]) > x_max:
-                x_max = int(l[1])
-           
-        elif l[0] == 'L':
-            x_dim -= int(l[1])
-        elif l[0] == 'U':
-            y_dim += int(l[1])
-        elif l[0] == 'D':
-            y_dim -= int(l[1])
+        dir = l[0]
+        move = int(l[1])
+
+
+        if dir == 'R':
+            x_pos += move
+            if x_pos > x_max:
+                x_max = x_pos
+
+        elif dir == 'L':
+            x_pos -= move
+
+        elif dir == 'U':
+            y_pos += move
+            if y_pos > y_max:
+                y_max = y_pos
+
+        elif dir == 'D':
+            y_pos -= move
+
+    print(f'Grid Size: [{x_max}, {y_max}]')
+
+    return [x_max+1, y_max+1]
+
+
+
+def print_state(x_pos, y_pos):
     
-    return [x_dim, y_dim]
+    for y in range(grid_size[0], -1, -1):
+        print('')
+        for x in range(grid_size[1]):
+            if x == x_pos and y == y_pos:
+                print('H', end='')
+            else:
+                print('.', end='')
+    print()
 
 
+def moves(lines):
+    x_pos = 0
+    y_pos = 0
+    print_state(x_pos, y_pos)
+
+    for line in lines:
+        
+        
+        l = line.split()
+        dir = l[0]
+        move = int(l[1])
+
+        if dir == 'R':
+            x_pos += move
+
+        elif dir == 'L':
+            x_pos -= move
+
+        elif dir == 'U':
+            y_pos += move
+
+        elif dir == 'D':
+            y_pos -= move
+        print_state(x_pos, y_pos)
 
 def main():
-    assert (len(sys.argv) > 1), 'Invalid CL Argument'
+    assert (len(sys.argv) == 2), 'Invalid CL Argument'
     data_file = str(sys.argv[1])
 
-    lines = parse_input(data_file)
-    print(lines)
+    instr = parse_input(data_file)
+    print(instr)
 
-    grid_size = get_grid_size(lines)
+    global grid_size
+    grid_size = get_grid_size(instr)
 
-    print(f'X:{grid_size[0]}, Y:{grid_size[1]}')
+    moves(instr)
+    
 
 if __name__ == '__main__':
     main()
