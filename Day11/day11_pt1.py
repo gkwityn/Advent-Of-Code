4ff63a5ac78cd03b0eac4b28666cd643b3d870ef
gkwityn <gkwityn@gmail.com>
2023-03-09 13:06:46 -0500

    fixing line parse

--- Day11/day11_pt1.py
+++ Day11/day11_pt1.py
@@ -13,19 +13,34 @@ class monkey():
 
 
 
-
+#TODO FIX line parse
 def parse_input():
+
+    monkeys = []
+
     with open("test.txt","r") as f:
         lines = f.readlines()
 
     for line in lines:
-        split_line = line.split(" ")
-        if split_line[0] == "Monkey":
-            monkey_number = int(split_line[1])
-        elif split_line[0] == "Operation":
-            
 
-    return lines
+        split_line = line.strip().split(" ")
+        if split_line[0] == "Monkey":
+            monkey_number = int(split_line[1].strip(":"))
+        elif split_line[0] == "Starting":
+            items = []
+        elif split_line[0] == "Operation":
+            opperation = split_line[1:]
+        elif split_line[0] == "Test":
+            divisible_by = int(split_line[3])
+        elif split_line[1] == "true":
+            if_true_throw_to = int(split_line[5])
+        elif split_line[1] == "false":
+            if_false_throw_to = int(split_line[5])
+        
+        monkey(monkey_number, items, opperation, divisible_by, if_true_throw_to, if_false_throw_to)
+        monkeys.append(monkey)
+            
+    return monkeys
 
 
 if __name__ == "__main__":
