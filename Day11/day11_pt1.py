d42d2a7a6ce38026c6e50a98c91adfa7a31f8a70
gkwityn <gkwityn@gmail.com>
2023-03-08 20:49:25 -0500

    building monkey obj to parse input to

--- Day11/day11_pt1.py
+++ Day11/day11_pt1.py
@@ -1,6 +1,16 @@
 class monkey():
-    def __init__(self):
-        pass
+    def __init__(self, monkey_number, items, 
+                 opperation, divisible_by, 
+                 if_true_throw_to, if_false_throw_to):
+        self.monkey_number = monkey_number
+        self.items = items
+        self.opperation = opperation
+        self.divisible_by = divisible_by
+        self.if_true_throw_to = if_true_throw_to
+        self.if_false_throw_to = if_false_throw_to
+
+        
+
 
 
 
@@ -8,6 +18,12 @@ def parse_input():
     with open("test.txt","r") as f:
         lines = f.readlines()
 
+    for line in lines:
+        split_line = line.split(" ")
+        if split_line[0] == "Monkey":
+            monkey_number = int(split_line[1])
+        elif split_line[0] == "Operation":
+            
 
     return lines
 
