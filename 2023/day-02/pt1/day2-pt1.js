d2989f12b2d3dd9d456171c997a8cc91e5629c01
gkwityn <gkwityn@gmail.com>
2024-02-18 01:00:37 -0500

    finished the game functionality, not returning right result

--- 2023/day-02/pt1/day2-pt1.js
+++ 2023/day-02/pt1/day2-pt1.js
@@ -1,9 +1,8 @@
-const { green } = require('color-name');
 
-function readFile(){
+function readFile(file){
     const fs = require('fs');
     try{
-        const contents = fs.readFileSync('./data-test.txt', 'utf8');
+        const contents = fs.readFileSync(file, 'utf8');
         const lines = contents.split('\n');
         return lines;
     }
@@ -23,28 +22,70 @@ function playGame(line){
     //Remove whitespace &
     //Split the game into each of the bag pulls
     const rounds = split[1].trim().split(';');
-    // console.log(bagPulls);
 
-    roundResults = {
+    let roundResults = {
         blue: 0,
         red: 0,
-        green: 0
+        green: 0,
+        reset: function(){
+            this.blue = 0;
+            this.red = 0;
+            this.green = 0;
+        }
     };
 
+    let roundCount = 1;
     for(let round of rounds){
         
-        // for(let pull of round.trim()){
-        //     console.log(pull);
-        // };
+        console.log(`\nround ${roundCount}:`);
 
+        //Break each round down to each color
+        //ex. '1 blue, 2 green'
+        let colors = round.trim().split(',');
 
+        //Increment the relevant color roundResult value
+        for(color of colors){
 
-        console.log(round);
-        // let colors = pull.split(',');
-        // console.log(colors);
+            let pull = color.trim().split(' ');
+            
+            let pullColor = pull[1];
+            let pullNumber = parseInt(pull[0]);
+            // console.log(`color: ${pullColor}, number: ${pullNumber}`)
+
+            switch(pullColor){
+                case 'blue':{
+                    if(pullNumber > roundResults.blue){
+                        roundResults.blue = pullNumber;
+                    }
+                    
+                }
+                case 'green':{
+                    if(pullNumber > roundResults.green){
+                        roundResults.green = pullNumber;
+                    }
+                }
+                case 'red':{
+                    if(pullNumber > roundResults.red){
+                        roundResults.red = pullNumber;
+                    }
+                }
+            }
+        }
+
+        console.log(roundResults);
+        roundCount++;                
     }
-   
-    
+
+    // 12 red cubes, 13 green cubes, and 14 blue
+    if( roundResults.red <= 12 && 
+        roundResults.green <= 13 && 
+        roundResults.blue <= 14){
+        return parseInt(split[0].split(' ')[1]);
+    }
+    else{
+        return 0;
+    }
+
 }
 
 
@@ -59,10 +100,19 @@ function main(){
     }
 
     const lines = readFile(process.argv[2]);
-    
+
+    let results = [];
     for(const line of lines){
-        playGame(line);
+        results.push(playGame(line));
     }
+
+
+    let sum = results.reduce((a,b) =>{
+        return a + b;
+    });
+    console.log(`\nFinal:`);
+    console.log(results);
+    console.log(`total sum: ${sum}`);
 
 
 }
