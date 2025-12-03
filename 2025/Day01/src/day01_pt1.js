694f0041a211078bcf2536f1de6db642f08a6caf
gkwityn <gkwityn@gmail.com>
2025-12-02 22:53:49 -0500

    2025- day 1 parsed input

--- 2025/Day01/src/day01_pt1.js
+++ 2025/Day01/src/day01_pt1.js
@@ -1,27 +1,48 @@
+const { EOF } = require('dns');
 
-
+require('console'); 
+console
 
 let readInput = () =>{
 
     const fs = require('fs');
-
     const filePath = process.argv[2];
 
     if(!filePath){
         console.error("Please provide the input file path as a command line argument.");
-        console.log(`\n--- Reading data from: ${filePath} ---`);
-        console.log('File Content:');
-        console.log(fileContent);
-        console.log('--------------------------------------\n');
         process.exit(1);
     }
+    else{        
+        try{
+            const data = fs.readFileSync(filePath, 'utf8');
+            return data;
+        }
+        catch(err){
+            console.error("Error reading the file:", err);
+            process.exit(1);
+        }
+    }        
+};
 
-    try{
-        const data = fs.readFileSync(filePath, 'utf8');
-        return data;
-    }
-    catch(err){
-        console.error("Error reading the file:", err);
-        process.exit(1);
+let main = () => {
+    data = readInput();
+    
+    // handles \n and \r\n
+    const readLines = (data) => data.split(/\r?\n/); 
+    const lines = readLines(data); 
+
+    let lineNum = 0
+    for (const line of lines) {
+        console.log(`Line ${lineNum++}:`);
+        console.log(line);
     }
 };
+
+main();
+
+
+
+
+
+
+
